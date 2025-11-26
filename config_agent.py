a2032582e397d458441c94d4ae473ad4f7155760
linjiaqiang <linjiaqiang@maitrust.com>
2025-11-26 14:28:30 +0800

    fix bug

--- config_agent.py
+++ config_agent.py
@@ -1,8 +1,14 @@
 from langchain_openai import ChatOpenAI
 from deepagents import create_deep_agent
 
+from context import MainContext
+from langgraph.runtime import Runtime
 
-async def create_config_agent():
+
+async def create_config_agent(runtime: Runtime[MainContext]):
+    configurable = runtime.get("configurable", {})
+    prompt = configurable.get("system_prompt", "You are a helpful AI assistant.")
+
     # 创建主 Agent
     model = ChatOpenAI(
         model="glm-4.6",
@@ -11,5 +17,7 @@ async def create_config_agent():
 
     main_agent = create_deep_agent(
         model=model,
+        system_prompt=prompt,
+        context_schema=MainContext,
     )
     return main_agent
---
+++ context.py
@@ -0,0 +1,44 @@
+"""Define the configurable parameters for the agent."""
+
+from typing import Annotated, Literal
+from pydantic import BaseModel, Field
+
+
+class AgentContext(BaseModel):
+    """The runtime context for the agent."""
+
+    system_prompt: str = Field(
+        default="You are a helpful AI assistant.",
+        description="The system prompt to use for the agent's interactions. "
+        "This prompt sets the context and behavior for the agent."
+    )
+
+    # model: Annotated[
+    #         Literal[
+    #             "anthropic:claude-haiku-4-5",
+    #             "anthropic:claude-sonnet-4-5",
+    #             "openai:gpt-5",
+    #             "openai:gpt-5-mini"
+    #         ],
+    #         {"__template_metadata__": {"kind": "llm"}},
+    #     ] = Field(
+    #         default="anthropic:claude-haiku-4-5",
+    #         description="The name of the language model to use for the agent's main interactions. "
+    #     "Should be in the form: provider/model-name."
+    # )
+    #
+    # selected_tools: list[Literal["finance_research", "advanced_research", "basic_research", "get_todays_date"]] = Field(
+    #     default = ["get_todays_date"],
+    #     description="The list of tools to use for the agent's interactions. "
+    #     "This list should contain the names of the tools to use."
+    # )
+
+    name: str = Field(
+        default="react_agent",
+        description="The name of the agent."
+    )
+
+
+class MainContext(AgentContext):
+    pass
+    # subagents: list[AgentContext] = Field(description="Subagents to use.")
